7398239fa8f102d4e2c9b812d357c5634850153c
tufedtm <tufedtm@gmail.com>
2016-02-17 18:50:20 +0300

    page   +urls    views      +index()      +good()

--- page/views.py
+++ page/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# coding=utf-8
+from django.http import HttpResponse
+from models import *
 
-# Create your views here.
+
+def index(request, cat_id):
+
+    if cat_id is None:
+        cat = Category.objects.first()
+    else:
+        cat = Category.objects.get(pk=cat_id)
+
+    goods = Good.objects.filter(category=cat).order_by('name')
+    s = u'Категория: ' + cat.name + '<br><br>'
+    for good in goods:
+        s = s + '(' + str(good.pk) + ') ' + good.name + '<br>'
+
+    return HttpResponse(s)
+
+
+def good(request, good_id):
+    good = Good.objects.get(pk=good_id)
+    s = good.name + '<br><br>' + good.category.name + '<br><br>' + good.description
+
+    if not good.in_stock:
+        s += '<br><br>' + 'Нет в наличии!'
+
+    return HttpResponse(s)
