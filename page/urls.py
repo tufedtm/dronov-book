ca67a6c17693d536214b495d6b9d7109b745c8be
tufedtm <tufedtm@gmail.com>
2016-02-17 22:30:49 +0300

    page   urls     fix index url pattern   html     replace urls to {% url %}

--- page/urls.py
+++ page/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^((?P<cat_id>\d+)/)?$', views.index, name='index'),
+    url(r'^(?:(?P<cat_id>\d+)/)?$', views.index, name='index'),
     url(r'^good/(?P<good_id>\d+)/$', views.good, name='good'),
 ]
