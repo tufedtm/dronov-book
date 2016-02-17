a2763a7579d60e2406844adf8a17bff03ba94a6c
tufedtm <tufedtm@gmail.com>
2016-02-17 20:16:14 +0300

    page    +html    views      replace HttpResponse to render

--- page/views.py
+++ page/views.py
@@ -1,9 +1,10 @@
 # coding=utf-8
-from django.http import HttpResponse
+from django.shortcuts import render
 from models import *
 
 
 def index(request, cat_id):
+    categories = Category.objects.all().order_by('name')
 
     if cat_id is None:
         cat = Category.objects.first()
@@ -11,19 +12,11 @@ def index(request, cat_id):
         cat = Category.objects.get(pk=cat_id)
 
     goods = Good.objects.filter(category=cat).order_by('name')
-    s = u'Категория: ' + cat.name + '<br><br>'
 
-    for item in goods:
-        s = s + '(' + str(item.pk) + ') ' + item.name + '<br>'
-
-    return HttpResponse(s)
+    return render(request, 'index.html', {'category': cat, 'categories': categories, 'goods': goods})
 
 
 def good(request, good_id):
     good_item = Good.objects.get(pk=good_id)
-    s = good_item.name + '<br><br>' + good_item.category.name + '<br><br>' + good_item.description
 
-    if not good_item.in_stock:
-        s += '<br><br>' + 'Нет в наличии!'
-
-    return HttpResponse(s)
+    return render(request, 'good.html', {'good_item': good_item})
