8e36838fceaab433e8fa3924c31556f7ba932e27
tufedtm <tufedtm@gmail.com>
2016-02-20 08:06:04 +0300

    +flatpages

--- dronov/settings.py
+++ dronov/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+
     'page'
 ]
 
--- dronov/urls.py
+++ dronov/urls.py
@@ -21,6 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^goods/', include('page.urls')),
+    url(r'^(?P<url>.*/)$', 'django.contrib.flatpages.views.flatpage')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
